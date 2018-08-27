778e7b750b5d75d7133dfc30e57062aec3db6fdb
eggsie <eggsie@Erics-MacBook-Pro.local>
2018-08-27 08:19:27 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,7 @@ var wins = 0;
 var losses = 0;
 var score = 0;
 
-
 var random = Math.floor((Math.random()* 100)+ 19);
-
 
 var crystal1 = Math.floor((Math.random() * 12) + 1);
 var crystal2 = Math.floor((Math.random() * 12) + 1);
